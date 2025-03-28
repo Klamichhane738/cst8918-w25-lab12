47c0047ec6b6268d6caa9f5b39fed19f25068797
Klamichhane738.Keshav <lami0053@algonquinlive.com>
2025-03-28 17:16:38 -0400

    Integration Test

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -31,3 +31,19 @@ resource "azurerm_storage_container" "app_container" {
   storage_account_name  = azurerm_storage_account.app_storage.name
   container_access_type = "private"
 }
+
+# Use the remote state output in your resources (example)
+resource "azurerm_storage_account" "remote_storage" {
+  name                     = data.terraform_remote_state.backend.outputs.backend_storage_account_name
+  resource_group_name      = "some-resource-group"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+}
+
+resource "azurerm_storage_container" "remote_container" {
+  name                  = data.terraform_remote_state.backend.outputs.backend_container_name
+  storage_account_name  = azurerm_storage_account.remote_storage.name
+  container_access_type = "private"
+}
